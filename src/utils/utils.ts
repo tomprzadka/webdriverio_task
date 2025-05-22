0830f7dea2a88ef228901d49bae16a7cc808f6bf
Thomas <przadkatomasz@gmail.com>
2025-05-22 04:31:41 +0200

    chore: disable animations - should be included in 429304a commit

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,3 +8,27 @@ export async function saveRecording(path: string): Promise<void> {
 export async function saveToAppiumLog(errorMessage: string): Promise<void> {
   await driver.logEvent('appium', `[CUSTOM_ERROR] ${errorMessage}`);
 }
+
+export async function disableAnimations(): Promise<void> {
+  await driver.execute('mobile:shell', {
+    command: 'settings put global window_animation_scale 0',
+  });
+  await driver.execute('mobile:shell', {
+    command: 'settings put global transition_animation_scale 0',
+  });
+  await driver.execute('mobile:shell', {
+    command: 'settings put global animator_duration_scale 0',
+  });
+}
+
+export async function restoreAnimations(): Promise<void> {
+  await driver.execute('mobile:shell', {
+    command: 'settings put global window_animation_scale 1',
+  });
+  await driver.execute('mobile:shell', {
+    command: 'settings put global transition_animation_scale 1',
+  });
+  await driver.execute('mobile:shell', {
+    command: 'settings put global animator_duration_scale 1',
+  });
+}
