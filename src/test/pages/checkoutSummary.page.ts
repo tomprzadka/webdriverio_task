c02f07332f7f1c314c843fc8f926e5ec1d42014e
Thomas <przadkatomasz@gmail.com>
2025-05-22 06:47:21 +0200

    fix: checkoutSummaryProductsList selector

--- src/test/pages/checkoutSummary.page.ts
+++ src/test/pages/checkoutSummary.page.ts
@@ -1,9 +1,17 @@
 import { Actions } from '../helpers/actions';
+import { SwipeOptions } from 'webdriverio';
 
 export class CheckoutSummaryPage {
   actions: Actions;
   constructor() {
     this.actions = new Actions();
+  }
+  get checkoutSummaryProductsList(): ChainablePromiseElement {
+    return $('~test-CHECKOUT: OVERVIEW');
+  }
+
+  get finishCheckoutButton(): ChainablePromiseElement {
+    return $('~test-FINISH');
   }
 
   getProductTitleName(productName: string): ChainablePromiseElement {
@@ -23,5 +31,18 @@ export class CheckoutSummaryPage {
 
   async getProductPrice(productName: string): Promise<string> {
     return this.actions.getElementText(this.getProductPriceTag(productName));
+  }
+  async swipeCheckoutList(options: SwipeOptions): Promise<void> {
+    const { direction, duration, percent } = options;
+    await this.actions.swipe({
+      direction,
+      duration,
+      percent,
+      scrollableElement: this.checkoutSummaryProductsList,
+    });
+  }
+
+  async completeCheckout(): Promise<void> {
+    await this.actions.click(this.finishCheckoutButton);
   }
 }
